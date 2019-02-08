325d146bc3ead6ffdc7e144aeae54f276f07f137
Eder Pires <ederpbj@gmail.com>
2019-02-07 22:17:34 -0300

    atualizar index com ejs

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
+//se ouver porta no ambiente ele usa, se não usa a 3000
+const port = process.env.PORT || 3000
+
+//Modo 1
+app.get('/', (requeste, response) => {
+    response.send('Olá Fullstack Lab')
+})
+
+/* Modo 2
 const resolver = (requeste, response) => {
     response.send('Olá Fullstack Lab')
 }
-
 app.get('/', resolver)
+*/
 
-app.listen(3000, (err)=>{
+app.listen(port, (err)=>{
     if(err){
         console.log('error')
     }else{
-        console.log('Como-fazer Server is running...')
+        console.log('Como-fazer Server is running on port:', port)
     }
 })
