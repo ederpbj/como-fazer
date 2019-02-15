4632a0318cad579f38d614344c022a33f96585ea
Eder Pires <ederpbj@gmail.com>
2019-02-15 17:48:22 -0300

    Criação do modulo Publicação

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 
 const categorias = require('./routes/categorias')
+const publicacoes = require('./routes/publicacoes')
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded())
@@ -18,6 +19,7 @@ app.get('/', async(requeste, response) => {
 })
 
 app.use('/categorias',categorias)
+app.use('/publicacoes',publicacoes)
 
 app.listen(port, (err)=>{
     if(err){
---
+++ routes/publicacoes.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+const controller = require('../controllers/publicacoes')
+
+
+router.get('/nova', controller.novaForm)
+
+router.post('/nova', controller.nova)
+
+router.get('/', controller.list) 
+
+router.get('/excluir/:id', controller.excluir)
+
+router.get('/editar/:id', controller.editarForm)
+
+router.post('/editar/:id', controller.editar)
+
+module.exports = router
